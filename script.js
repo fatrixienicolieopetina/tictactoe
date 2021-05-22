ac9db81c17f73851f33b1912a3b92dc88a997a92
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-05-22 13:37:28 +0800

    Added normal playing logic

--- script.js
+++ script.js
@@ -7,54 +7,99 @@ const TicTacToe = (function() {
     const GameBoard = (function() {
         let gridItems = Array(9).fill('I');
 
-        function setValue(index) {
+        function resetBoard() {
+            gridItems = Array(9).fill('I');
+        }
+
+        function setGridItemValue(index) {
             gridItems[index] = turn;
         }
 
         function checkWinner() {
-            //check row
+            checkRowWin();
+            checkColumnWin();
+            checkDiagonalWin();
+
+            if(checkDraw()) {
+                DisplayController.updateInfo(`Tis a Draw`);
+            }
+        }
+
+        function checkRowWin() {
             for(let i = 0; i < gridItems.length; i+=3) {
                 if((gridItems[i] == gridItems[i+1] 
                     && gridItems[i+1] == gridItems[i+2]) 
-                    && gridItems[0] != 'I') {
-                    alert("winner");
+                    && gridItems[i] != 'I') {
+                    DisplayController.changeBackgroundOfWinningCombination(i,
+                        i+1, i+2);
+                    broadCastWinner();
                 }
             }
+        }
 
-            //check column
+        function checkColumnWin() {
             for(let i = 0 ; i < (gridItems.length)/3; i++) {
                 if((gridItems[i] == gridItems[i+3] 
                     && gridItems[i+3] == gridItems[i+6])
                     && gridItems[i] != 'I') {
-                    alert("winner");
+                    DisplayController.changeBackgroundOfWinningCombination(i, i+3, i+6);
+                    broadCastWinner();
+                }
+            }
+        }
+
+        function checkDiagonalWin() {
+            let leftDiagonalWin = gridItems[0] == gridItems[4] 
+                    && gridItems[4] == gridItems[8] 
+                    && gridItems[0] != 'I';
+            let rightDiagonalWin = gridItems[2] == gridItems[4] 
+                    && gridItems[4] == gridItems[6] 
+                    && gridItems[2] != 'I'
+
+            if(leftDiagonalWin || rightDiagonalWin) {
+                if(leftDiagonalWin) {
+                    DisplayController.changeBackgroundOfWinningCombination(0, 4, 8);
+                } else if(rightDiagonalWin) {
+                    DisplayController.changeBackgroundOfWinningCombination(2, 4, 6);
+                }
+                broadCastWinner();
+            }
+        }
+
+        function getPreviousTurn() {
+            if(turn == 'X') return playerO.playerName;
+            else if(turn == 'O') return playerX.playerName;
+        }
+
+        function checkDraw() {
+            for(let i = 0; i < gridItems.length; i++) {
+                if(gridItems[i] == 'I') {
+                    return false;
                 }
             }
 
-            //check diagonals
-            if((gridItems[0] == gridItems[4] 
-                && gridItems[4] == gridItems[8] 
-                && gridItems[0] != 'I')|| 
-                (gridItems[2] == gridItems[4] 
-                    && gridItems[4] == gridItems[6] 
-                    && gridItems[0] != 'I')) {
-                    alert("winning")
-            }
-
+            return true;
         }
 
-        function itemsHaveTheSameValue(items) {
-            return items.every((val, i, arr) => val === arr[0] )
+        function broadCastWinner() {
+            requestAnimationFrame(() => {
+                setTimeout(() => {
+                    DisplayController.updateInfo(`${getPreviousTurn()} WINS`);
+                    DisplayController.removeListeners();
+                });
+            });
         }
 
         return {
-            setValue,
-            checkWinner
+            setGridItemValue,
+            checkWinner,
+            checkDraw,
+            resetBoard
         }
     }());
 
 
     const DisplayController = (function() {
-        let latestMove = '';
         function initDisplay() {
             let boardItems = document.getElementsByClassName("board-item");
 
@@ -63,7 +108,28 @@ const TicTacToe = (function() {
                     processPlayerMove, {once: true});
                 
             }
+
+            turn = 'X';
         }
+
+        function resetDisplay() {
+            let boardItems = document.getElementsByClassName("board-item");
+
+            for(let i = 0; i < boardItems.length; i++) {
+                boardItems[i].setAttribute('class', 'board-item');
+            }
+
+            initDisplay();
+        }
+
+        function removeListeners() {
+            let boardItems = document.getElementsByClassName("board-item");
+
+            for(let i = 0; i < boardItems.length; i++) {
+                boardItems[i].removeEventListener('click', processPlayerMove);
+            }
+        }
+
 
         function processPlayerMove() {
             displayPlayerMove(this);
@@ -74,21 +140,39 @@ const TicTacToe = (function() {
         function displayPlayerMove(element) {
             element.innerText = turn;
             element.setAttribute('class', 'board-item colored');
-            GameBoard.setValue(parseInt(element.id));
+            GameBoard.setGridItemValue(parseInt(element.id));
         }
 
         function switchTurn(currentTurn) {
-            if(currentTurn == 'X') {
-                turn = 'O'
-                playerO.makeAmove();
-            } else if (currentTurn == 'O') {
-                turn = 'X';
-                playerX.makeAmove();
+            if(!GameBoard.checkDraw()) {
+                if(currentTurn == 'X') {
+                    turn = 'O';
+                    playerO.makeAmove();
+                } else if (currentTurn == 'O') {
+                    turn = 'X';
+                    playerX.makeAmove();
+                } 
             }
         }
 
+        function updateInfo(infoText) {
+            let info = document.getElementById("info");
+            info.innerText = infoText;
+        }
+
+        function changeBackgroundOfWinningCombination(index1, index2, index3) {
+            let boardItems = document.getElementsByClassName('board-item');
+            boardItems[index1].setAttribute('class', 'board-item winner');
+            boardItems[index2].setAttribute('class', 'board-item winner');
+            boardItems[index3].setAttribute('class', 'board-item winner');
+        }
+
         return {
-            initDisplay
+            initDisplay,
+            resetDisplay,
+            updateInfo,
+            removeListeners,
+            changeBackgroundOfWinningCombination
         }
     })();
 
@@ -97,22 +181,31 @@ const TicTacToe = (function() {
         playerName,
         moveType,
         makeAmove() {
-            alert(`${playerName} makes a move`)
-            /**
-             * To-do : Add logic here for making a player move based on moveType
-             */
+            requestAnimationFrame(() => {
+                setTimeout(() => {
+                    DisplayController.updateInfo(`${playerName} should make a move.`);
+                });
+              });
         }
     });
 
-    function setTurn() {
-
+    function addRestartEventListener() {
+        document.getElementById("restart-btn").addEventListener('click', function() {
+            requestAnimationFrame(() => {
+                setTimeout(() => {
+                    DisplayController.updateInfo('X should make a move');
+                    GameBoard.resetBoard();
+                    DisplayController.resetDisplay();
+                });
+            });
+        });
     }
 
     function start() {
         playerX = createPlayer({playerName: 'XXX', moveType: 'X'});
         playerO = createPlayer({playerName: 'OOO', moveType: 'O'});
         DisplayController.initDisplay();
-        playerX.makeAmove();
+        addRestartEventListener();
     }
 
     return {start};
